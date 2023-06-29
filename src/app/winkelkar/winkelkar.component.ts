3909f4aea0d9da010f5accc523e5338b8a384cba
Eman Kalloub <eman.kalloub@vdabcampus.be>
2023-06-29 13:00:32 +0200

    afgewerkt met verbetering

--- src/app/winkelkar/winkelkar.component.ts
+++ src/app/winkelkar/winkelkar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component ,  Input  } from '@angular/core';
+import { WinkelkarService } from '../winkelkar.service';
+import { WinkelKarContent } from '../winkelkarContent';
+import { Groenten } from '../groenten';
 
 @Component({
   selector: 'app-winkelkar',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./winkelkar.component.css']
 })
 export class WinkelkarComponent {
+  @Input() winkelmandje!:  Groenten;
+  
+  Bestellingen: WinkelKarContent[] = [];
+
+  
+  constructor(private winkelkarService : WinkelkarService) {}
+
+  ngOnInit(): void {
+    this.getBestellingen();
+    
+  }
+
+  getBestellingen(): void {
+    this.winkelkarService.getBestellingen()
+        .subscribe(bestellingen => this.Bestellingen = bestellingen);
+  }
+
+  
 
 }
